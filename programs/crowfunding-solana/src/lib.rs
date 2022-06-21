511f45a4231c3dc52b513d5e53524e952f798bb1
David Zabaleta <david.zabaleta@publicisgroupe.com>
2022-06-20 20:47:25 -0500

    Create campaign functionality created

--- programs/crowfunding-solana/src/lib.rs
+++ programs/crowfunding-solana/src/lib.rs
@@ -4,12 +4,34 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod crowfunding_solana {
+    use anchor_lang::solana_program::entrypoint::ProgramResult;
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create(context: Context<Create>, name: String, description: String) -> ProgramResult {
+        let campaign = &mut context.accounts.campaign;
+
+        campaign.name = name;
+        campaign.description = description;
+        campaign.amount_donated = 0;
+        campaign.admin = *context.accounts.user.key;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Create<'info> {
+    #[account(init, payer=user, space=9000, seeds=[b"CAMPAIGN_DEMO".as_ref(), user.key().as_ref()], bump)]
+    pub campaign: Account<'info, Campaign>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>
+}
+
+#[account]
+pub struct Campaign {
+    pub admin: Pubkey,
+    pub name: String,
+    pub description: String,
+    pub amount_donated: u64
+}
